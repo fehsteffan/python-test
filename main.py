d552323f5b2dc24122351dd64f407a50ccf4c006
Felipe Steffan <felip.developer@hotmail.com>
2022-11-03 22:40:22 -0300

    exercicios python

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# abrir os 6 arquivos em excel
+
+lista_meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
+
+for mes in lista_meses:
+    tabela_vendas = pd.read_excel(f'{mes}.xlsx')
+    if (tabela_vendas['Vendas'] > 55000).any():
+        vendedor = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendedor'].values[0]
+        vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendas'].values[0]
+        print(f'No mes {mes} Alguem bateu a meta: Vendedor: {vendedor}, Vendas: {vendas}')
+
+
+
+
+
+
+
